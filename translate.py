28934a43e4d849412e53ef61ee60c84f7925356b
Celestin-Pet <celestin.np@gmail.com>
2023-09-17 17:43:01 +0200

    language translate

---
+++ translate.py
@@ -0,0 +1,5 @@
+from translate import Translator
+translator = Translator(to_lang = "kinyarwanda")
+translation = translator.translate("dog")  # Enter the text you want to translate in side the brackets
+
+print(translation)
